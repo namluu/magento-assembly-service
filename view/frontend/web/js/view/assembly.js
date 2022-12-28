3f6826a9cface7c23024463f3c979a7fc4b2b057
Nam Luu <nam.luuduc@gmail.com>
2022-12-28 17:26:21 +0700

    add event action assembly items and calculate price

--- view/frontend/web/js/view/assembly.js
+++ view/frontend/web/js/view/assembly.js
@@ -2,12 +2,21 @@ define([
     'uiComponent',
     'ko',
     'Magento_Customer/js/customer-data',
+    'Magento_Catalog/js/price-utils',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/full-screen-loader'
 ], function (
     Component,
     ko,
-    customerData
+    customerData,
+    priceUtils,
+    quote,
+    fullScreenLoader
 ) {
     'use strict';
+
+    var pricePerProduct = 30;
+    var maxPriceDelivery = 90;
 
     function Item(id, qty, name, image, selected = false) {
         this.id       = ko.observable(id);
@@ -24,6 +33,9 @@ define([
 
         isDisplayed: ko.observable(true),
         productList: ko.observableArray([]),
+        productSaved: ko.observableArray([]),
+        totalPrice: ko.observable(0),
+        statusAction: ko.observable('before_select'),
 
         initObservable: function () {
             var self = this._super();
@@ -37,6 +49,7 @@ define([
             var self = this;
             this._super();
             this.renderProducts();
+            this.totalPrice = ko.computed(self.computedTotalPrice , this);
         },
 
         renderProducts: function () {
@@ -56,6 +69,77 @@ define([
                         );
                     }
                 });
+            }
+        },
+
+        productSelected: function() {
+            return this.productList().filter(function (prod) {
+                return prod.selected();
+            });
+        },
+
+        computedTotalPrice: function () {
+            var total = 0,
+                dataList = [],
+                self = this;
+
+            if (self.statusAction() == 'selected') {
+                dataList = self.productSelected();
+            } else if (self.statusAction() == 'saved') {
+                dataList = self.productSaved();
+            }
+
+            dataList.forEach(function(item) {
+                total += item.qty() * pricePerProduct;
+                if (total > maxPriceDelivery) {
+                    total = maxPriceDelivery;
+                }
+            });
+
+            return self.getFormattedPrice(total);
+        },
+
+        getFormattedPrice: function (price) {
+            return priceUtils.formatPrice(price, quote.getPriceFormat());
+        },
+
+        triggerAction: function (action, data, event) {
+            var self = this;
+            fullScreenLoader.startLoader();
+
+            switch (action) {
+                case 'save':
+                    self.saveAction();
+                    break;
+                case 'cancel':
+                    self.cancelAction();
+                    break;
+                default:
+                    self.selectAction();
+                    break;
+            }
+            fullScreenLoader.stopLoader();
+        },
+
+        selectAction: function () {
+            var self = this;
+            self.statusAction('selected');
+            self.productList().map((item) => {
+                if (item.selected()) item.selected(false);
+            });
+        },
+
+        saveAction: function () {
+            var self = this;
+            self.statusAction('saved');
+        },
+
+        cancelAction: function () {
+            var self = this;
+            if (self.productSaved().length) {
+                self.statusAction('saved');
+            } else {
+                self.statusAction('before_select');
             }
         }
     });
