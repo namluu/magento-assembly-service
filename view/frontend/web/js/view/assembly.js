85f914415d297c6d4d99ef9eee2d9ff4ae9a7e1d
Nam Luu <nam.luuduc@gmail.com>
2022-12-28 17:25:24 +0700

    render assembly product items

--- view/frontend/web/js/view/assembly.js
+++ view/frontend/web/js/view/assembly.js
@@ -1,11 +1,21 @@
 define([
     'uiComponent',
-    'ko'
+    'ko',
+    'Magento_Customer/js/customer-data',
 ], function (
     Component,
-    ko
+    ko,
+    customerData
 ) {
     'use strict';
+
+    function Item(id, qty, name, image, selected = false) {
+        this.id       = ko.observable(id);
+        this.qty      = ko.observable(qty);
+        this.name     = ko.observable(name);
+        this.image    = ko.observable(image);
+        this.selected = ko.observable(selected);
+    }
 
     return Component.extend({
         defaults: {
@@ -13,6 +23,7 @@ define([
         },
 
         isDisplayed: ko.observable(true),
+        productList: ko.observableArray([]),
 
         initObservable: function () {
             var self = this._super();
@@ -25,6 +36,27 @@ define([
         initialize: function () {
             var self = this;
             this._super();
+            this.renderProducts();
+        },
+
+        renderProducts: function () {
+            var products = customerData.get('cart')().items,
+                self = this;
+
+            if (products.length > 0) {
+                products.forEach(function(product) {
+                    if (product.product_type != 'virtual') {
+                        self.productList.push(
+                            new Item(
+                                product.product_id,
+                                product.qty,
+                                product.product_name,
+                                product.product_image
+                            )
+                        );
+                    }
+                });
+            }
         }
     });
 });
